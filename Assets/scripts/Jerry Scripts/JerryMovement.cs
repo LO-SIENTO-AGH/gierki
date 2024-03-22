cdb349142ed789476047048b2dc278c88109e250
Anna Franczyk <anamonium@gmail.com>
2024-03-22 21:23:12 +0100

    added jumping to jerry mouse

--- Assets/scripts/Jerry Scripts/JerryMovement.cs
+++ Assets/scripts/Jerry Scripts/JerryMovement.cs
@@ -8,6 +8,14 @@ public class JerryMovement : MonoBehaviour
     public float speed = 5f;
     private Rigidbody2D myBody;
     private Animator animator;
+
+    public Transform groundCheckPosition;
+
+    public LayerMask groundLayer;
+
+    private bool isGrounded;
+    private bool jumped;
+    private float jumpPower = 5f;
    
     void Awake(){
         myBody = GetComponent<Rigidbody2D> ();
@@ -19,9 +27,9 @@ public class JerryMovement : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update(){
+        CheckIfGrounded();
+        PlayerJump();
     }
 
     void FixedUpdate(){
@@ -33,9 +41,11 @@ public class JerryMovement : MonoBehaviour
 
         if(h > 0){
             myBody.velocity = new Vector2(speed, myBody.velocity.y);
+            ChangeDirection(1);
         }
         else if(h < 0){
             myBody.velocity = new Vector2(-speed, myBody.velocity.y);
+            ChangeDirection(-1);
         }
         else{
             myBody.velocity = new Vector2(0f, myBody.velocity.y);
@@ -43,4 +53,31 @@ public class JerryMovement : MonoBehaviour
 
         animator.SetInteger("Speed", Mathf.Abs((int)myBody.velocity.x));
     }
+
+    void ChangeDirection(int direction){
+        Vector3 tempScale = transform.localScale;
+        tempScale.x = direction;
+        transform.localScale = tempScale;
+    }
+
+    void CheckIfGrounded(){
+        isGrounded = Physics2D.Raycast(groundCheckPosition.position, Vector2.down, 0.1f, groundLayer);
+
+        if(isGrounded){
+            if(jumped){
+                jumped = false;
+                animator.SetBool("Jump", false);
+            }
+        }
+    }
+
+   void PlayerJump(){
+        if(isGrounded){
+            if(Input.GetKey(KeyCode.Space)){
+                jumped = true;
+                myBody.velocity = new Vector2(myBody.velocity.x, jumpPower);
+                animator.SetBool("Jump", true);
+            }
+        }
+   }
 }
